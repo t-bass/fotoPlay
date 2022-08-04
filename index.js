953f0aaa2f7aabdd6ac130ce1c3731ad46f9b82d
Виктор Гончаров <t-bass@mail.ru>
2022-08-05 01:39:09 +0300

    рефакторинг библиотечки

--- index.js
+++ index.js
@@ -1,57 +1,91 @@
-document.querySelector('.player-chunk-prev').addEventListener('click', function () {
-  moveClass('player-chunk-active', 'previousElementSibling');
-  moveClass('timeline-chunk-active', 'previousElementSibling', (el) => {
-    const inner = el.querySelector('.timeline-chunk-inner');
-    w = parseFloat(inner.style.width) || 0;
+function initPlayer(params) {
+  const target = document.querySelector(params.target);
 
-    el.querySelector('.timeline-chunk-inner').style.width = '';
-    return w <= 30;
+  if (target === null || params.slides === undefined) {
+    return;
+  }
+  let timelineChunks = '',
+    playerChunks = '';
+
+  let isFirst = true;
+
+  for (const el of params.slides) {
+    timelineChunks += `
+  <div class="timeline-chunk ${isFirst ? 'timeline-chunk-active' : ''}">
+    <div class="timeline-chunk-inner"></div>
+  </div>`;
+
+    playerChunks += `
+  <div class="player-chunk ${isFirst ? 'player-chunk-active' : ''}">
+    <img src="${el.url}" alt="${el.alt || ''}" />
+  </div>`;
+
+    isFirst = false;
+  }
+  target.innerHTML = `
+<div class="player">
+  <div class="timeline">${timelineChunks}</div>
+  <div class="player-content-wrapper">
+    <div class="player-chunk-swither player-chunk-prev"></div>
+    <div class="player-chunk-swither player-chunk-next"></div>
+    <div class="player-content">${playerChunks}</div>
+  </div>
+</div>`;
+
+  target.querySelector('.player-chunk-prev').addEventListener('click', function () {
+    moveClass('player-chunk-active', 'previousElementSibling');
+    moveClass('timeline-chunk-active', 'previousElementSibling', (el) => {
+      const inner = el.querySelector('.timeline-chunk-inner');
+      w = parseFloat(inner.style.width) || 0;
+
+      el.querySelector('.timeline-chunk-inner').style.width = '';
+      return w <= 30;
+    });
   });
-});
+  target.querySelector('.player-chunk-next').addEventListener('click', next);
 
-document.querySelector('.player-chunk-next').addEventListener('click', next);
+  function moveClass(className, method, pred) {
+    const active = target.querySelector('.' + className),
+      next = active[method];
 
-function moveClass(className, method, pred) {
-  const active = document.querySelector('.' + className),
-    next = active[method];
+    if (pred && !pred(active)) {
+      return null;
+    }
 
-  if (pred && !pred(active)) {
+    if (next) {
+      active.classList.remove(className);
+      next.classList.add(className);
+      return active;
+    }
     return null;
   }
 
-  if (next) {
-    active.classList.remove(className);
-    next.classList.add(className);
-    return active;
-  }
-  return null;
-}
+  function next() {
+    moveClass('player-chunk-active', 'nextElementSibling');
+    const el = moveClass('timeline-chunk-active', 'nextElementSibling');
 
-function next() {
-  moveClass('player-chunk-active', 'nextElementSibling');
-  const el = moveClass('timeline-chunk-active', 'nextElementSibling');
-
-  if (el) {
-    el.querySelector('.timeline-chunk-inner').style.width = '';
-  }
-}
-
-let timer;
-function runInterval(time, step) {
-  clearInterval(timer);
-  timer = setInterval(() => {
-    const active = document
-      .querySelector('.timeline-chunk-active')
-      .querySelector('.timeline-chunk-inner');
-
-    const w = parseFloat(active.style.width) || 0;
-
-    if (w === 100) {
-      next();
-      return;
+    if (el) {
+      el.querySelector('.timeline-chunk-inner').style.width = '';
     }
+  }
 
-    active.style.width = String(w + step) + '%';
-  }, (time * 1000 * step) / 100);
+  let timer;
+  function runInterval(time, step) {
+    clearInterval(timer);
+    timer = setInterval(() => {
+      const active = target
+        .querySelector('.timeline-chunk-active')
+        .querySelector('.timeline-chunk-inner');
+
+      const w = parseFloat(active.style.width) || 0;
+
+      if (w === 100) {
+        next();
+        return;
+      }
+
+      active.style.width = String(w + step) + '%';
+    }, (time * 1000 * step) / 100);
+  }
+  runInterval(params.delay || 1, 1);
 }
-runInterval(5, 1);
